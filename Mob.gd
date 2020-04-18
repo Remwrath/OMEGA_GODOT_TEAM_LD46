e295d751a1e0c8711846cda8853322c565f5b75e
gioele <gioele.santi@gmail.com>
2020-04-18 13:38:17 +0200

    Added steering to Mob
    Mouse steering with follow and reach click behavior.
     Added a func react on NPC and a chant method on Mob that needs to be implemented to loop through nearby NPCs

--- Mob.gd
+++ Mob.gd
@@ -1,5 +1,14 @@
+extends KinematicBody2D
 #this is for controlling the mob as an entity NPC's in turn interact with this mob
-extends Node2D
+
+#steering vars
+export var max_speed: = 120.0
+export var slow_radius: = 30.0
+var _velocity: = Vector2.ZERO
+const DISTANCE_THRESHOLD: = 3.0
+
+var target_position: Vector2
+var track_cursor := false
 
 #stats holder for the mob
 var stats = {"speed":100}
@@ -11,19 +20,51 @@ var members = []
 var state = "idle"
 
 func _ready():
-	pass # Replace with function body.
+	set_physics_process(false)
+
+# MOVEMENT 
+
+func _unhandled_input(event):
+	#Mouse steering
+	if event is InputEventMouseButton:
+		if event.pressed: #follow cursor
+			track_cursor = true
+			set_physics_process(true)
+		if event.is_action_released('click'): #reach click point
+			#a target object may be placed on map upon mouse release
+			track_cursor = false
+			target_position = get_global_mouse_position() #GUI will add target on objective
+			set_physics_process(true)
+
+func _physics_process(delta):
 	
-func _input(event):
-   if event is InputEventMouseMotion:
-	   self.position = event.position
+	if track_cursor:
+		target_position = get_global_mouse_position()
+	var dist = global_position.distance_to(target_position)
+	
+	if dist < DISTANCE_THRESHOLD:
+		set_physics_process(false)
+		return
+	
+	_velocity = Steering.arrive_to(
+		_velocity,
+		global_position,
+		target_position,
+		max_speed,
+		slow_radius)
+	
+	_velocity = move_and_slide(_velocity)
+
+#GROUP
+
+func chant(message):
+	#for npc in bodies in ChantArea call 
+	pass
 
 func gain_member(npc):
 	members.append(npc)
 
 func lose_member(npc):
-	pass
-
-func move(direction:Vector2):
 	pass
 
 #changes the mobs state and acts accordingly	

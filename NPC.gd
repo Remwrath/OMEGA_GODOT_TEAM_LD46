c2869737c18e83d544993f2d890f0c8360b82b24
Remwrath <reach.me.et@gmail.com>
2020-04-18 11:29:57 +0200

    randomize timers and use npc types

--- NPC.gd
+++ NPC.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
 # dictionary for any stats that may vary from NPC to NPC
-var stats = { "speed" : 100 }
+var stats = { "speed" : 100, "type" : Global.NPC_INSTIGATOR, "in_mob" : false}
+
 # *current* velocity of the NPC, used to move the NPC during _physics_process. To manually move the NPC, set this instead of calling a move function directly.
 var velocity = Vector2.ZERO
 
@@ -16,17 +17,33 @@ func _ready():
 	# Each timer should start the other so the NPC alternates between moving and standing still
 	$MoveTimer.connect("timeout", $WaitTimer, "start")
 	$WaitTimer.connect("timeout", $MoveTimer, "start")
+	
+	#randomise the timers
+	$MoveTimer.wait_time = rand_range(0.0, 2.0)
+	$WaitTimer.wait_time = rand_range(0.0, 2.0)
 
 func _physics_process(delta):
 	# move the NPC by whatever the velocity was set to in other functions
 	move_and_slide(velocity)
+	if stats.type == Global.NPC_INSTIGATOR:
+		join_mob();
+
+func _process(delta):
+	if stats.in_mob:
+		velocity = (get_mob().position - self.position).normalized() * stats["speed"]
+
+func get_mob():
+	var mob = get_node("/root/map/Mob")
+	return mob
 
 #call to make the npc join the mob
 func join_mob():
-	pass
+	#Global.get_mob().gain_member(self);
+	self.stats.in_mob = true
 
 #call to make the npc leave the mob
 func leave_mob():
+	self.stats.in_mob = false
 	pass
 
 #call to make the npc consider joining the mob
@@ -34,6 +51,8 @@ func consider_joining_mob():
 	pass
 
 func start_move():
+	if stats.in_mob:
+		return
 	# start moving the NPC in a random cardinal direction. This could be easily changed to moving in a completely random direction if people would prefer.
 	var dir = randf() # generate a random number to determine which of four directions we will go in
 	# divide the possible values of dir into four equal parts, and assign a direction to each

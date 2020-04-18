c96e38c86ef3fa6438b11098197e6006d1b16e8a
Mike Ahearn <mjahearn13@alum.mit.edu>
2020-04-18 18:23:30 -0400

    tilemap added, NPCs downscaled to actual default

--- NPC.gd
+++ NPC.gd
@@ -20,17 +20,17 @@ var Slogan = [
 ]
 
 var type_stats = {
-	Type.INSTIGATOR : {"speed" : 100, "commitment" : 100, "in_mob" : true},
-	Type.PAWN : {"speed" : 50},
-	Type.DEMIHUMAN : {"speed" : 100},
-	Type.CLERIC : {"speed" : 100},
-	Type.ELF : {"speed" : 100},
-	Type.ALCHEMIST : {"speed" : 100},
-	Type.SORCERER : {"speed" : 100},
+	Type.INSTIGATOR : {"speed" : 25, "commitment" : 100, "in_mob" : true},
+	Type.PAWN : {"speed" : 12.5},
+	Type.DEMIHUMAN : {"speed" : 25},
+	Type.CLERIC : {"speed" : 25},
+	Type.ELF : {"speed" : 25},
+	Type.ALCHEMIST : {"speed" : 25},
+	Type.SORCERER : {"speed" : 25},
 }
 
 var in_mob = false
-var speed = 100
+var speed = 12.5
 var type = Type.PAWN
 var commitment = 0
 var trigger_slogans = []
@@ -43,9 +43,9 @@ var stats = {}
 var velocity = Vector2.ZERO
 var target = Vector2.ZERO
 
-var roam_radius = 150
-var slow_radius = 15
-var arrive_distance = 70
+var roam_radius = 37.5
+var slow_radius = 2.75
+var arrive_distance = 17.5
 var follow = false
 
 
--- map.gd
+++ map.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+# reference to level is saved as a variable so levels can be swapped out without needing to alter references elsewhere in code
+onready var level = $TestLevel
+
 func _ready():
 	# Thinking of an instagator as a NPC type that is always a mob member (there should only be one).
-	add_npc(Vector2(100, 100), NPC.Type.INSTIGATOR)
-	add_npc(Vector2(100, 200), NPC.Type.INSTIGATOR)
-	add_npc(Vector2(100, 300), NPC.Type.INSTIGATOR)
+	add_npc(Vector2(-25, 25), NPC.Type.INSTIGATOR)
+	add_npc(Vector2(-25, 50), NPC.Type.INSTIGATOR)
+	add_npc(Vector2(-25, 75), NPC.Type.INSTIGATOR)
 	generate_random_npcs(10)
 
 
@@ -19,4 +22,14 @@ func add_npc(position: Vector2, type):
 func generate_random_npcs(amount):
 	for _i in range(amount):
 		var type = NPC.Type.DEMIHUMAN if randf() < 0.5 else NPC.Type.CLERIC
-		add_npc(Vector2(rand_range(1, 800), rand_range(1, 800)), type)
+		var npc_pos = Vector2(rand_range(1, 200), rand_range(1, 200))
+		#if the NPC would spawn inside a wall, pick a different spawn point
+		while not is_valid_spawn_point(npc_pos):
+			npc_pos = Vector2(rand_range(1, 200), rand_range(1, 200))
+		add_npc(npc_pos, type)
+
+# checks whether a chosen spawn point is inside a wall
+func is_valid_spawn_point(coords):
+	var tile_coords = level.get_node("Tilemap").world_to_map(coords)
+	var cell = level.get_node("Tilemap").get_cellv(tile_coords)
+	return (cell == -1) # if the cell has an index of -1, it's empty and therefore a valid spawn point
